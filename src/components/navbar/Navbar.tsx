220757315cef72d571a0e2b1731266fded396179
Sandy <machadogitt@gmail.com>
2025-01-27 11:35:52 -0300

    Adicionando react router dom

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
@@ -5,7 +7,7 @@ function Navbar() {
             			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                <Link to='/home' className="text-2xl font-bold">Farmácia</Link>
 
                     <div className='flex gap-4'>
                         Produtos
